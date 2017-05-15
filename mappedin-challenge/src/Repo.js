fada651413d052ba2b74588cda405c5449df2726
Shibisoma <shibisoma@hotmail.com>
2017-05-15 12:30:15 -0400

    refactoring css

--- mappedin-challenge/src/Repo.js
+++ mappedin-challenge/src/Repo.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import './Repo.css';
+
 class Repo extends Component {
   constructor(props) {
     super(props);
@@ -17,19 +19,25 @@ class Repo extends Component {
   render() {
     return(
       <div className="Repo">
-        <h2>
-          <i className="fa fa-star"></i>
-          <a href={this.state.htmlUrl}>{this.state.fullName}</a>
+        <h2 className="repo-header">
+          <i className="fa fa-star repo-icon"></i>
+          <a className="repo-link" href={this.state.htmlUrl}>
+            {this.state.fullName}
+          </a>
         </h2>
         <p>{this.state.description}</p>
         <div className="counters">
           <div className="stars">
-            <i className="fa fa-star"></i>
-            <a href={this.state.starUrl}>{this.state.starCount}</a>
+            <i className="fa fa-star star-icon"></i>
+            <a className="star-link" href={this.state.starUrl}>
+              {this.state.starCount}
+            </a>
           </div>
           <div className="forks">
-            <i className="fa fa-code-fork"></i>
-            <a href={this.state.forkUrl}>{this.state.forkCount}</a>
+            <i className="fa fa-code-fork fork-icon"></i>
+            <a className="fork-link" href={this.state.forkUrl}>
+              {this.state.forkCount}
+            </a>
           </div>
         </div>
       </div>
